c869d82240e0d571467ad55635f41e712c957a8e
MohammedGamalll <11410120220283@stud.cu.edu.eg>
2025-03-16 02:07:38 +0200

    add initial project setup with Tailwind CSS and Flowbite integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './core/components/sidebar/sidebar.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    // RouterOutlet,
+    SidebarComponent,
+    FooterComponent,
+    MainLayoutComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'portfolio';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,52 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./pages/home/home.component').then((m) => m.HomeComponent),
+      },
+      {
+        path: 'skills',
+        loadComponent: () =>
+          import('./pages/skills/skills.component').then(
+            (m) => m.SkillsComponent
+          ),
+      },
+      {
+        path: 'projects',
+        loadComponent: () =>
+          import('./pages/projects/projects.component').then(
+            (m) => m.ProjectsComponent
+          ),
+      },
+      {
+        path: 'contact',
+        loadComponent: () =>
+          import('./pages/contact/contact.component').then(
+            (m) => m.ContactComponent
+          ),
+      },
+      {
+        path: 'education',
+        loadComponent: () =>
+          import('./pages/education/education.component').then(
+            (m) => m.EducationComponent
+          ),
+      },
+    ],
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/notfound/notfound.component').then(
+        (m) => m.NotfoundComponent
+      ),
+  },
+];
