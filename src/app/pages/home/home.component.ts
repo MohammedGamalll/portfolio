d04c543b0889deb2c492e487daf9254d48cf3737
MohammedGamalll <11410120220283@stud.cu.edu.eg>
2025-04-26 19:02:43 +0300

    feat: update footer and navbar components, enhance home and projects sections with new images and styles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,11 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  Inject,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import TWriter from 't-writer.js';
 
@@ -9,29 +16,32 @@ import TWriter from 't-writer.js';
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements AfterViewInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
   ngAfterViewInit() {
-    const target = document.querySelector('.tw');
-    const writer = new TWriter(target, {
-      loop: true,
-      typeSpeed: 80,
-      deleteSpeed: 80,
-      typeColor: '#808080',
-    });
-    writer
-      .type('Software Engineer')
-      .rest(1000)
-      .changeOps({ deleteSpeed: 20 })
-      .remove(17)
-      .type('Frontend Developer (Angular)')
-      .rest(1000)
-      .remove(30)
-      .type('Backend Developer (ASP.NET Core)')
-      .rest(1000)
-      .changeOps({ deleteSpeed: 20 })
-      .remove(32)
-      .type('Full Stack Developer')
-      .rest(1000)
-      .clear()
-      .start();
+    if (isPlatformBrowser(this.platformId)) {
+      const target = document.querySelector('.tw');
+      const writer = new TWriter(target, {
+        loop: true,
+        typeSpeed: 80,
+        deleteSpeed: 80,
+        typeColor: '#808080',
+      });
+      writer
+        .type('Software Engineer')
+        .rest(1000)
+        .changeOps({ deleteSpeed: 20 })
+        .remove(17)
+        .type('Frontend Developer (Angular)')
+        .rest(1000)
+        .remove(30)
+        .type('Backend Developer (ASP.NET Core)')
+        .rest(1000)
+        .changeOps({ deleteSpeed: 20 })
+        .remove(32)
+        .type('Full Stack Developer')
+        .rest(1000)
+        .clear()
+        .start();
+    }
   }
 }
